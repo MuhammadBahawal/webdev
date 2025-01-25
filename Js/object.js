c455e1560d5b056360e918ed30ddce946cef724c
Muhammad Bahawal <167578686+MuhammadBahawal@users.noreply.github.com>
2025-01-25 20:45:07 +0500

    updated

---
+++ Js/object.js
@@ -0,0 +1,53 @@
+
+
+let num = prompt("enter the number")
+if(num >8){
+    console.log("hello")
+}else if(num<8){
+    document.write("hello how are you")
+}else{
+
+}
+
+switch (key) {
+    case 1:
+        alert("what")
+        break;
+    case 2:
+        alert("what is you name")
+    default:
+
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let student = {
+//     Name : "Adeel",
+//     Roll_no:"fsd fl 239",
+//     Class:"BSSE"
+// }
+// console.log(student);
+// studen.semester = "4th";
+// console.log(student);
+
+// let a = 10 ;
+// let b = 7;
+// let c = a>b?(a+b):(a-b)
+// alert(c)
